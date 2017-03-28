a99190f6976bbaa1b8f4ba29dff0126a212589bd
brett poarch <brett.poarch@gmail.com>
2017-03-27 20:05:26 -0700

    working basic

--- basic/main.js
+++ basic/main.js
@@ -1,42 +1,11 @@
 $(document).ready(function(){
-	var sOne = true
-	var sTwo = true
-	var sThree = true
-
-	$(".hOne").on("click", function(){
-		if(sOne){
-			$(".one").addClass("selector")
-			$(".pOne").addClass("pHide")	
-			sOne = false
+	$('a').on('click', function(e){
+		e.preventDefault()
+		if($(this).find('+ section').hasClass('open')){
+			$('section').removeClass('open')
 		} else {
-			$(".one").removeClass("selector")
-			$(".pOne").removeClass("pHide")	
-			sOne = true
+			$('section').removeClass('open')
+			$(this).find('+ section').addClass('open')
 		}
 	})
-	
-	$(".hTwo").on("click", function(){
-		if(sTwo){
-			$(".two").addClass("selector")
-			$(".pTwo").addClass("pHide")
-			sTwo = false
-		} else {
-			$(".two").removeClass("selector")
-			$(".pTwo").removeClass("pHide")
-			sTwo = true
-		}
-	})
-
-	$(".hThree").on("click", function(){
-		if(sThree){
-			$(".three").addClass("selector")
-			$(".pThree").addClass("pHide")
-			sThree = false
-		} else {
-			$(".three").removeClass("selector")
-			$(".pThree").removeClass("pHide")
-			sThree = true
-		}
-	})
-
 })
